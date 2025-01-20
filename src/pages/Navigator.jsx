f7958ce74632e445befdeb058db8dffc4620cd0f
Snehal <snehalkulkarni009@gmail.com>
2025-01-20 09:49:16 +0530

    Removed charts from admin dashboard and navigate customer page to profile

--- src/pages/Navigator.jsx
+++ src/pages/Navigator.jsx
@@ -29,7 +29,7 @@ function Navigator() {
     {
       title: "Customer",
       description: "Handle customer-related activities",
-      path: "/login",
+      path: "/profile",
     },
     {
       title: "Sales",
