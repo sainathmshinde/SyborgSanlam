6b70c7628bc63bd0660922bc70f403903b8a89e3
Snehal <snehalkulkarni009@gmail.com>
2025-02-24 10:28:18 +0530

    Changes in Customer

--- src/pages/EditLeadManagement.jsx
+++ src/pages/EditLeadManagement.jsx
@@ -294,13 +294,13 @@ const EditLeadManagement = () => {
                 className="px-4 py-2 -mb-px text-sm font-medium text-center border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300"
               >
                 Key Contributors
-              </TabsTrigger>
+              </TabsTrigger> */}
               <TabsTrigger
                 value="documents"
                 className="px-4 py-2 -mb-px text-sm font-medium text-center border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300"
               >
                 Upload Documents
-              </TabsTrigger> */}
+              </TabsTrigger>
             </TabsList>
           </div>
           <div className="overflow-auto max-h-[500px]">
